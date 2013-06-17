01e22e3f41787d3cd96bf92ab48e30191fe80b06
Arunan Skanthan <arunan.skanthan@gmail.com>
2013-06-18 07:54:30 +1000

    Looping through dictionaries

--- for_loop_dict.py
+++ for_loop_dict.py
@@ -1,5 +1,12 @@
-person = {'name': "Tim", 'color': 'blue', 'hair': 'brown'}
+person = {'name': "Tim", 'color': 'blue', 'hair': 'brown',
+          'interests': ['cycling', 'running', 'biking']}
 
 for key in person:  # iterate
-  print "key is " + key + " value is " + person[key]
+  if (key == 'interests'):
+    print "Interests ..."
+    for interest in person[key]:
+      print "\tinterest is " + interest
+
+  else:
+    print "key is " + key + " value is " + person[key]
 
